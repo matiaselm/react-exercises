6cfa3396f2e5b828daf5ab516bed947f818ff4e0
Matias Jalava <matias@elm.fi>
2020-06-12 16:04:27 +0300

    changed the workspace to be able to contain multiple exercises dynamically

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import App from './exercise1/App'; // Change exercise-number depending on the app you want to run
 
 ReactDOM.render(
   <React.StrictMode>
