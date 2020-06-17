e29fdf8404454ac1a97d5d4926872d22d5c3680d
Matias Jalava <matias@elm.fi>
2020-06-17 14:02:17 +0300

    removed unnessesary app.css

--- src/exercise1/App.js
+++ src/exercise1/App.js
@@ -1,27 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './styles/style.css';
 import { CounterScreen, HeaderBox, CarList } from './components/Components.js'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
 
         <HeaderBox header="Counter1" paragraph="Click the buttons to change the number" />
         <CounterScreen />
         <CarList language='fi' />
 
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div >
   );
