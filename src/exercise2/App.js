0b3d29ca95af058defc0faa21f476b6efaf57b7a
Matias Jalava <matias@elm.fi>
2020-06-17 14:16:00 +0300

    initial commit for exercise 2

---
+++ src/exercise2/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles/style.css';
+import { } from './components/Components.js'
+
+function App() {
+    return (
+        <div className="App">
+            <p>Yoooo</p>
+        </div >
+    );
+}
+
+export default App;
