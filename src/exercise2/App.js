990fed8c9283fe195d4436dc7ca3f3520bfc9696
Matias Jalava <matias@elm.fi>
2020-07-10 12:35:13 +0300

    broken commit to move work to another machine

--- src/exercise2/App.js
+++ src/exercise2/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './styles/style.css';
-import { InputField } from './components/Components.js'
+import { UserTable } from './components/Components.js'
 
 function App() {
     return (
         <div className="App">
-            <InputField />
+            <UserTable />
         </div >
     );
 }
--- src/exercise2/components/Components.js
+++ src/exercise2/components/Components.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 /*
 Nippu B (15p)
@@ -20,17 +20,95 @@ Nippu B (15p)
     > Kun käyttäjä painaa OK-painiketta, viestikomponentti poistuu näkyviltä.
 */
 
-const InputField = (props) => {
+// InputField contains 3 input fields for the user to fill; their name, address and city. After saving their info, it is saved to the array
+
+let userList = []
+
+const InputField = () => {
+
+    const nameField = document.getElementById('name')
+    const addressField = document.getElementById('address')
+    const cityField = document.getElementById('city')
+
+    const saveUser = () => {
+        const clearFields = () => {
+            nameField.value = ''
+            addressField.value = ''
+            cityField.value = ''
+        }
+
+        if (nameField.value.length > 0 && addressField.value.length > 0 && cityField.value.length > 0) {
+
+            userList.unshift({
+                name: `${nameField.value}`,
+                address: `${addressField.value}`,
+                city: `${cityField.value}`
+            })
+
+            clearFields()
+            console.log(`userList: ${userList[0].name}`)
+        } else {
+            alert('Please input text to all of the fields')
+        }
+    }
 
     const inputField = (
         <>
-            <p>This here will be an input field</p>
+            <input type="text" id="name" name="name" placeholder="Name" /><br></br>
+            <input type="text" id="address" name="address" placeholder="Address" /><br></br>
+            <input type="text" id="city" name="city" placeholder="City" /><br></br>
+            <input type="submit" value="Save" onClick={saveUser} />
         </>
     )
 
     return inputField
 }
 
+const UserInfo = (props) => {
+
+    console.log(`UserInfo props: ${props}`)
+
+    return (
+        <tr>
+            <td>{props.user.name}</td>
+            <td>{props.user.address}</td>
+        </tr>
+    )
+}
+
+const UserMap = (props) => {
+
+    console.log(`UserMap list: ${props.userlist}`)
+
+    return (
+        <>
+            <table>
+                <tbody>
+                    <tr><th>Name</th><th>Address</th></tr>
+                    {
+                        props.userlist.map((user) =>
+                            <UserInfo user={user} />
+                        )
+                    }
+                </tbody>
+            </table>
+        </>
+    )
+}
+
+const UserTable = () => {
+
+    const [list, setList] = useState([])
+
+    setList(userList)
+
+    return (
+        <>
+            <InputField />
+            <UserMap userlist={list} />
+        </>
+    )
+}
 export {
-    InputField
+    UserTable
 }
