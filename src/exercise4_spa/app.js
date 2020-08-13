d36ffaf40e2f9bc07cd3a86186c244846a9998c5
Matias Jalava <matias@elm.fi>
2020-08-13 14:17:00 +0300

    minor changes

--- src/exercise4_spa/app.js
+++ src/exercise4_spa/app.js
@@ -2,6 +2,12 @@ import React from 'react';
 import RouterMenu from './components/RouterMenu';
 import { UserProvider } from './contexts/UserContext';
 
+/*
+    The app works as is asked in assignment, at least I think so. 
+    There's no real functionality behind the buttons and there's some hard-coded things, like admin.
+    I intend to remove those hard-coded things and add button functionality on the next assignment.
+*/
+
 const App = () => {
     return (
         <div className="App">
